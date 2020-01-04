29101d94ff7d7c01fda0fad1aeb67df7c4eda6b2
Olakunle Boye <kunlexzy@gmail.com>
2020-01-04 01:57:39 +0100

    able to create event and push into events variable

--- app.js
+++ app.js
@@ -5,15 +5,30 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json())
 
 app.use('/graphql', graphqlHttp({
 	schema: buildSchema(`
+		type Event {
+			_id: ID!
+			title: String!
+			description: String!
+			price: Float!
+			date: String!
+		}
+		input EventIput {
+			title: String!
+			description: String!
+			price: Float!
+			date: String!
+		}
 		type RootQuery {
-				events: [String!]!
+				events: [Event!]!
 		}
 		type RootMutation {
-				createEvent(name: String): String
+				createEvent(eventInput: EventIput): Event
 		}
 		schema {
 			query: RootQuery
@@ -22,11 +37,18 @@ app.use('/graphql', graphqlHttp({
 		`),
 	rootValue: {
 		events: () => {
-			return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
+			return events;
 		},
 		createEvent: (args) => {
-			const eventName = args.name;
-			return eventName;
+			const event = {
+				_id: Math.random().toString(),
+				title: args.eventInput.title,
+				description: args.eventInput.description,
+				price: args.eventInput.price,
+				date: args.eventInput.date
+			}
+			events.push(event)
+			return event;
 		}
 	},
 	graphiql: true
